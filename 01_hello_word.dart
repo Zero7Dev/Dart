8db5591c4c88540b5ad90faf44f524d65c428cb3
Julio <juliocoloma1875@gmail.com>
2024-03-08 16:55:01 -0500

    Inicios en dart

---
+++ 01_hello_word.dart
@@ -0,0 +1,14 @@
+void main() {
+  var myName = 'Julio';
+  String myNames = 'Julio';
+  final mysNames='hola mi amor';
+  late final names;
+    const myNamess='Julio';
+//   tiempo de construccion 
+//   final tiempo de ejecucion 
+  myName.toUpperCase();
+    print('hola $myName');
+  }
+
+  
+  
